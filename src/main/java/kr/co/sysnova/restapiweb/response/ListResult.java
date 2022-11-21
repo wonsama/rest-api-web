977aafea3006ddae531805ea7c17e4f4aab77f98
park won <parkwon@impconsulting.co.kr>
2022-11-21 15:59:08 +0900

    api 구성하기

---
+++ src/main/java/kr/co/sysnova/restapiweb/response/ListResult.java
@@ -0,0 +1,11 @@
+package kr.co.sysnova.restapiweb.response;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ListResult<T> extends CommonResult {
+    private List<T> list;
+}
---
+++ src/main/java/kr/co/sysnova/restapiweb/service/ResponseService.java
@@ -0,0 +1,56 @@
+package kr.co.sysnova.restapiweb.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.sysnova.restapiweb.response.CommonResponse;
+import kr.co.sysnova.restapiweb.response.CommonResult;
+import kr.co.sysnova.restapiweb.response.ListResult;
+import kr.co.sysnova.restapiweb.response.SingleResult;
+
+@Service
+public class ResponseService {
+
+    // 단건
+    public <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 다건
+    public <T> ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 성공 결과만
+    public CommonResult getSuccessResult() {
+        CommonResult result = new CommonResult();
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 실패 결과만
+    public CommonResult getFailResult() {
+        CommonResult result = new CommonResult();
+        setFailResult(result);
+        return result;
+    }
+
+    private void setSuccessResult(CommonResult result) {
+        result.setSuccess(true);
+        result.setCode(CommonResponse.SUCCESS.getCode());
+        result.setMsg(CommonResponse.SUCCESS.getMsg());
+    }
+
+    private void setFailResult(CommonResult result) {
+        result.setSuccess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMsg(CommonResponse.FAIL.getMsg());
+    }
+}
